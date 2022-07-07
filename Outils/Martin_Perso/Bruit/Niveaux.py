8ab550790c5b0e29c529b44e90c65c35f0cb2911
Martin_boulot <martin.schoreisz@cerema.fr>
2022-07-07 15:32:20 +0200

    ajout module niveaux dans Bruit ; ajout connexion bdd test_valorisation

--- Outils/Martin_Perso/Bruit/Niveaux.py
+++ Outils/Martin_Perso/Bruit/Niveaux.py
@@ -5,12 +5,12 @@ Created on 25 mai 2022
 @author: martin.schoreisz
 module de manipulation des niveaux de bruit
 '''
-from math import log10
+from math import log10, sqrt
 
 pRef = 20*pow(10,-6) 
 
 
-def Niveau2Pression(NiveauBruit):
+def niveau2Pression(NiveauBruit):
     """
     transformer un niveau de pression en dB en pression en Pascal
     in : 
@@ -20,7 +20,7 @@ def Niveau2Pression(NiveauBruit):
     """
     return pRef*pow(10,NiveauBruit/20)
 
-def Pression2Niveau(pression):
+def pression2Niveau(pression):
     """
     transformer une pression efficace en pascal en niveau en db
     in : 
@@ -29,3 +29,14 @@ def Pression2Niveau(pression):
         NiveauBruit : reel positif
     """
     return 20*log10(pression/pRef)
+
+
+def moyenneQuadratiquePression(iterablePression):
+    """
+    calculer la moyenne quadratique de pression. c'est la pression efficace moyenne
+    in : 
+        iterablePression : numeric : iterable de pression
+    out :
+        moyennequadratqPression : numeric : moyenne quadratique de pression
+    """ 
+    return sqrt((1/len(iterablePression))*sum([pow(i, 2) for i in iterablePression]))
