affc2c1d2776a6f1bd162610706b1633ac7822bc
Martin_boulot <martin.schoreisz@cerema.fr>
2022-10-13 18:05:21 +0200

    ajout module meteo, ajout TNI

---
+++ Outils/Martin_Perso/Bruit/Meteo.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 30 sept. 2022
+
+@author: martin.schoreisz
+Module de calcul lie aux donnees meteo
+'''
+
+
+from Outils import checkAttributsinDf, checkParamValues
+from math import  log, cos, sqrt, radians
+from statistics import mean
+import pandas as pd
+import numpy as np
+
+listAttributObligatoireDfDonneesCapteur = ['vts_vent_haut', 'vit_vent_bas', 'temp_haut', 'temp_bas', 'date_heure']
+dfAngle = pd.DataFrame({'angle_min':[e for e in range(0, 361, 30)][:-1], 'angle_max':[e for e in range(0, 361, 30)][1:]})
+nomAttrPenteTemp = 'a_t'
+nomAttrPenteVtsVent = 'a_w'
+nomAttrGradVts = 'gradient_vits'
+nomAttrGradTemp = 'gradient_temp'
+coeffConversionCelsiusKelvin = 274.15
+
+
+def calculGradientVitesseSon(temp, gradientTemp, gradientVitesse, angle):
+    """
+    calcul du gradeint de vitesse du son, à une hauteur donnée.
+    in : 
+        temp : float : temperature
+        gradientTemp : float : gradeint de température
+        gradientVitesse : float : gradient de vitesse
+        angle : float : angle en degre
+    """
+    try:
+        return (10.0407 / sqrt(temp) * gradientTemp) + (gradientVitesse * cos(radians(angle)))
+    except ValueError:
+        return np.nan
+    
+    
+def conditionPropagation(gradientCeleriteSon, hauteur=3):
+    checkParamValues(hauteur, [3, 6, 10])
+    if hauteur <= 4.5:
+        if -0.2 <= gradientCeleriteSon < -0.015:
+            return "défavorable"
+        elif -0.2 > gradientCeleriteSon:
+            return "très défavorable"
+        elif 0.2 >= gradientCeleriteSon > 0.015:
+            return "favorable"
+        elif 0.2 < gradientCeleriteSon :
+            return "très favorable"
+        else:
+            return "homogene"
+    elif hauteur > 8:
+        if -0.1 <= gradientCeleriteSon < -0.007:
+            return "défavorable"
+        elif -0.1 > gradientCeleriteSon:
+            return "très défavorable"
+        elif 0.1 > gradientCeleriteSon > 0.007:
+            return "favorable"
+        elif 0.1 < gradientCeleriteSon :
+            return "très favorable"
+        else:
+            return "homogene"
+    else:
+        if -0.15 <= gradientCeleriteSon < -0.01:
+            return "défavorable"
+        elif -0.15 > gradientCeleriteSon:
+            return "très défavorable"
+        elif 0.15 > gradientCeleriteSon > 0.01:
+            return "favorable"
+        elif 0.15 < gradientCeleriteSon :
+            return "très favorable"
+        else:
+            return "homogene"
+    
+
+class DonneesMeteo(object):
+    """
+    classe permettant le traitement des données météo. est basée sur une 
+    dataframe contenant les infos fournies par les capteurs : 
+    vts_vent_haut, vts_vent_bas, temp_haut, temp_bas, date_heure
+    attribut : 
+        dfCapteurs : dataframe de base
+        dfPente : df des capteurs avec les pentes a_t et a_w
+        hauteurBas : integer : hauteur des capteurs bas
+        hauteurHaut : hauteur des capteurs haut
+        pasAgregTemporel : integer : pas d'agregation des données mesurées en minutes
+        dateHeureDeb : string : date et heure de début à prendre en comte, format YYYY-mm-dd HH:MM:SS
+        a_w : pente de la variation de vitesse du vent selon la hauteur
+        a_t : pente de la variation de température selon la hauteur
+    """
+    
+    def __init__(self, dfCapteurs, hauteurBas, hauteurHaut, pasAgregTemporel, dateHeureDeb):
+        """
+        vérfication initiale des données
+        """
+        checkAttributsinDf(dfCapteurs, listAttributObligatoireDfDonneesCapteur)
+        self.dfCapteurs = dfCapteurs
+        self.hauteurBas = hauteurBas
+        self.hauteurHaut = hauteurHaut
+        self.pasAgregTemporel = pasAgregTemporel
+        self.dateHeureDeb = dateHeureDeb
+        self.ajoutTempKelvin(self.dfCapteurs)
+        self.dfPente = self.calculPente(self.moyennerTemporelDfCapteur())
+        
+        
+    def ajoutTempKelvin(self, df):
+        df['temp_haut_k'] = df['temp_haut'] + coeffConversionCelsiusKelvin
+        df['temp_bas_k'] = df['temp_bas'] + coeffConversionCelsiusKelvin
+        return
+        
+        
+    def moyennerTemporelDfCapteur(self):
+        """
+        agregation temporalle des donnees sources
+        """ 
+        return self.dfCapteurs.set_index('date_heure').resample(f'{self.pasAgregTemporel}T',
+                                                                origin=self.dateHeureDeb).mean().reset_index()
+        
+    
+    def calculPente(self, df):
+        """
+        calculer a_w et a_t, les pentes de la regression log linéaire de la vitesse et de la température par raport au sol 
+        pour chaque occurence de la df de base.
+        in : 
+            df : donnees en entree, en general issu demoyennerTemporelDfCapteur
+        out : 
+            a_t : float : pente de la température
+            a_w : float : pente de la vitesse du vent
+        """
+        df[nomAttrPenteVtsVent] = ((df.vts_vent_haut - df.vit_vent_bas) / (
+            log(self.hauteurHaut) - log (self.hauteurBas)))
+        df[nomAttrPenteTemp] = ((df.temp_haut_k - df.temp_bas_k) / (
+            log(self.hauteurHaut) - log (self.hauteurBas)))
+        return df
+        
+        
+    def calculGradient(self, df, hauteurCalcul):
+        """
+        à partir du calcul des pentes, calculer les gradients à une hauteur données.Modification 'sur place' de la Df en entrée
+        in : 
+            df : donnees en entree, en general issu demoyennerTemporelDfCapteur. Doit contenir les attributs a_t et a_w
+            hauteurCalcul : hauteur à laquelle sont déterminé les conditions de propagation
+        """
+        df = df.copy()
+        checkAttributsinDf(df, [nomAttrPenteVtsVent, nomAttrPenteTemp])
+        df[nomAttrGradVts] = df[nomAttrPenteVtsVent] / hauteurCalcul
+        df[nomAttrGradTemp] = df[nomAttrPenteTemp] / hauteurCalcul
+        return df
+    
+    
+    def analyseAngulaire(self, dfGradient):
+        """
+        pour chaque secteur angulaire, fournir le gradient de célérité du son.
+        modif sur place
+        in : 
+           dfGradient : dataframe issue de  calculGradient
+        """
+        checkAttributsinDf(dfGradient, listAttributObligatoireDfDonneesCapteur + 
+                           [nomAttrGradVts, nomAttrGradTemp])
+        dfAngleGradient = dfGradient.merge(dfAngle, how='cross')
+        dfAngleGradient['grad_min'] = dfAngleGradient.apply(lambda x: calculGradientVitesseSon(x.temp_bas_k, x[nomAttrGradTemp], 
+                                                                                 x[nomAttrGradVts], x.angle_min), axis=1)
+        dfAngleGradient['grad_max'] = dfAngleGradient.apply(lambda x: calculGradientVitesseSon(x.temp_haut_k, x[nomAttrGradTemp], 
+                                                                                     x[nomAttrGradVts], x.angle_max), axis=1)
+        dfAngleGradient['grad_moy'] = dfAngleGradient.apply(lambda x: calculGradientVitesseSon(mean([x.temp_haut_k, x.temp_bas_k]),
+                                                                                               x[nomAttrGradTemp],
+                                                                                               x[nomAttrGradVts],
+                                                                                               mean([x.angle_max, x.angle_min]))
+        , axis=1)   
+        return dfAngleGradient
+        
+        
+    
+        
+        
--- Outils/Martin_Perso/Bruit/Niveaux.py
+++ Outils/Martin_Perso/Bruit/Niveaux.py
@@ -6,8 +6,28 @@ Created on 25 mai 2022
 module de manipulation des niveaux de bruit
 '''
 from math import log10, sqrt
+from Outils import checkParamValues, checkAttributsinDf
+import pandas as pd
 
 pRef = 20*pow(10,-6) 
+sourceTypeAgregTemporalleList = ['pression', 'leq_a']
+
+
+def calculLden(laeq618, laeq1822, laeq226):
+    """
+    calculé le Lden a partirdes niveaux moyen sur les periodes reglementaires
+    """
+    return 10*log10((12/24*10**(laeq618/10)) + (4/24*10**((laeq1822+5)/10)) + (4/24*10**((laeq226+10)/10)))
+
+
+def calculTNI(l10, l90):
+    """
+    le trafic noise index
+    in : 
+        l10 : bruit dépassé 10 % du temps.
+        l90 : bruit dépassé 90% du temp
+    """
+    return 4 * (l10-l90) + l90 - 30
 
 
 def niveau2Pression(NiveauBruit):
@@ -40,3 +60,58 @@ def moyenneQuadratiquePression(iterablePression):
         moyennequadratqPression : numeric : moyenne quadratique de pression
     """ 
     return sqrt((1/len(iterablePression))*sum([pow(i, 2) for i in iterablePression]))
+
+
+def agregationTemporelle(horoDateDebut, horodateFin, pasTemporelMinute, dfDonneesBrutes, sourceType, reglementaire=False):
+    """
+    agreger les données issue de recupDonneesAcoustiqueSiteInstru().
+    in : 
+        pasTemporelMinute : integer. Le pas d'agrégation temporelle des données.
+        dfDonneesBrutes : dataframe.doit contenir un attribut 'pression' de type numeric décrivant la pression sonore
+        sourceType : string. Parmi 'pression' ou 'leq_a' selon l'attribut deja presentdans la df source
+        horoDateDebut : date ou horodate au format YYYY-mm-dd ou YYYY-mm-dd HH:MM:SS
+        horodateFin : date ou horodate au format YYYY-mm-dd ou YYYY-mm-dd HH:MM:SS
+        reglementaire : boolean. si true alors pasTemporelMinute est ignores pour agreges selon les periodes 6-18, 18-22, 22-6
+    out : 
+        dfMin : dataframe agregee avec les atributs pression et leq_a correspondants
+    """
+    # creation d'un index de date
+    if not reglementaire:
+        if pd.to_datetime(horodateFin) < pd.to_datetime(horoDateDebut):
+            raise ValueError('la date de fin et antérieure à la date de début ou ce sont les mêmes')
+        indexDate = pd.date_range(horoDateDebut, horodateFin, freq=f'{pasTemporelMinute}T')
+    else:
+        indexDate = indexPeriodesReglementaires(horoDateDebut, horodateFin)
+    dfDonneesBrutesCopy = dfDonneesBrutes.copy()
+    checkParamValues(sourceType, sourceTypeAgregTemporalleList)
+    if sourceType == 'pression':
+        checkAttributsinDf(dfDonneesBrutesCopy, 'pression')
+        dfDonneesBrutesCopy['leq_a'] = dfDonneesBrutesCopy.pression.apply(lambda x: pression2Niveau(x))
+    elif sourceType == 'leq_a':
+        checkAttributsinDf(dfDonneesBrutesCopy, 'leq_a')
+        dfDonneesBrutesCopy['pression'] = dfDonneesBrutesCopy.leq_a.apply(lambda x: niveau2Pression(x))
+    # sur des périodes de X minutes
+    dfMin = dfDonneesBrutesCopy.groupby(indexDate[indexDate.searchsorted(dfDonneesBrutesCopy.set_index('date_heure').index)]).agg(
+        pression=pd.NamedAgg(column="pression", aggfunc=lambda x: moyenneQuadratiquePression(x)),
+        leq_a=pd.NamedAgg(column="pression", aggfunc=lambda x: pression2Niveau(moyenneQuadratiquePression(x))))
+    # mise en forme des donnees 6 minutes
+    dfMin.index.rename('date_heure', inplace=True)
+    dfMin.reset_index(inplace=True)
+    return dfMin
+
+
+def indexPeriodesReglementaires(horoDateDebut, horodateFin):
+    """
+    sortir un index des periodes reglementaires sur une période
+    in :
+        horoDateDebut : date ou horodate au format YYYY-mm-dd ou YYYY-mm-dd HH:MM:SS
+        horodateFin : date ou horodate au format YYYY-mm-dd ou YYYY-mm-dd HH:MM:SS
+    out : 
+        pandas datetimeIndex par période reglementaire
+    """
+    if pd.to_datetime(horodateFin) <= pd.to_datetime(horoDateDebut):
+        raise ValueError('la date de fin et antérieure à la date de début ou ce sont les mêmes')
+    return pd.DatetimeIndex(pd.concat([pd.Series(pd.date_range(horoDateDebut, horodateFin)) +
+                                                       pd.Timedelta(hours=h) for h in (6, 18, 22)]
+                                                      ).sort_values().reset_index(drop=True))
+    
