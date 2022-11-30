8c6c54debb5fc08f99f811d40ea21b00fa0554f1
Martin_boulot <martin.schoreisz@cerema.fr>
2022-11-30 14:53:26 +0100

    typo dans trafic_Routier

--- Outils/src/Trafic/Trafic_Routier.py
+++ Outils/src/Trafic/Trafic_Routier.py
@@ -12,15 +12,15 @@ from Outils.Outils import checkAttributsinDf
 from datetime import datetime
 
 
-def graphDebitVitesse1jour(dfDebitVitesseVoiesBddWide, jour, NomVoies, sens, largeur=390, hauteur=200): 
+def graphDebitVitesse1jour(dfDebitVitesseVoiesBddWide, jour, NomVoies, sens, largeur=390, hauteur=320): 
     """
     cr��er un graph fondamental sur les voies lente, m�diane, rapide pour un jour donn�e
-    pour un compteur donn�e
+    pour un compteur donnee
     in:
-        dfDebitVitesseVoiesBddWide : dataframe des trafic et d�bit, avec attributs date_heure, voie, sens, TV, Vmoy_TV
+        dfDebitVitesseVoiesBddWide : dataframe des trafic et debit, avec attributs date_heure, voie, sens, TV, Vmoy_TV
         jour : integer: dayofyear
         NomVoies liste de string  des noms de voies � consid�rer
-        sens : string: nom du sens consid�r�
+        sens : string: nom du sens considere
         largeur : larguer des charts en pixel
         hauteur : hauteur des charts en pixel
     """
@@ -29,32 +29,32 @@ def graphDebitVitesse1jour(dfDebitVitesseVoiesBddWide, jour, NomVoies, sens, lar
                                      .assign(heure=dfDebitVitesseVoiesBddWide.date_heure.dt.hour)
                                      .loc[(dfDebitVitesseVoiesBddWide.date_heure.dt.dayofyear == jour) & (dfDebitVitesseVoiesBddWide['voie'] == voie) &
                                                                    (dfDebitVitesseVoiesBddWide['sens'] == sens)],
-                                    title = [f"{datetime.strptime('2022' + '-' + str(jour).rjust(3, '0'), '%Y-%j').strftime('%Y-%m-%d')} - jour n�{jour}",
+                                    title = [f"{datetime.strptime('2022' + '-' + str(jour).rjust(3, '0'), '%Y-%j').strftime('%Y-%m-%d')} - jour n°{jour}",
                                              f"Code Canal = {voie} : {sens}"],
-                                   width=largeur, height=250).mark_circle(size=100).encode(
-                x=alt.X('TV', title='D�bit'),
+                                   width=largeur, height=hauteur).mark_circle(size=100).encode(
+                x=alt.X('TV', title='Débit'),
                 y=alt.Y('Vmoy_TV', title='Vitesse'),
                 color=alt.Color('heure:N', scale=alt.Scale(scheme='turbo'))) + 
                            alt.Chart(dfDebitVitesseVoiesBddWide.loc[(dfDebitVitesseVoiesBddWide.date_heure.dt.dayofyear == jour) 
                                                                      & (dfDebitVitesseVoiesBddWide['voie'] == voie) &
                                                                    (dfDebitVitesseVoiesBddWide['sens'] == sens)],
-                                    title = [f"{datetime.strptime('2022' + '-' + str(jour).rjust(3, '0'), '%Y-%j').strftime('%Y-%m-%d')} - jour n�{jour}",
+                                    title = [f"{datetime.strptime('2022' + '-' + str(jour).rjust(3, '0'), '%Y-%j').strftime('%Y-%m-%d')} - jour n°{jour}",
                                              f"Code Canal = {voie} : sens : inter"],
-                                   width=largeur, height=250).mark_line(strokeWidth=0.5, color='gray').encode(
-                x=alt.X('TV', title='D�bit'),
+                                   width=largeur, height=hauteur).mark_line(strokeWidth=0.5, color='gray').encode(
+                x=alt.X('TV', title='Débit'),
                 y=alt.Y('Vmoy_TV', title='Vitesse'),
                 order='date_heure:T')) for voie in NomVoies])
     
     
 def graphDebitVitesseplusieursjour(dfDebitVitesseVoiesBddWide, listJour, NomVoies, sens, largeur=390, hauteur=200):     
     """
-    cr��er un graph fondamental sur les voies lente, m�diane, rapide pour plsueiurs jour donn�e
-    pour un compteur donn�e
+    cr��er un graph fondamental sur les voies lente, m�diane, rapide pour plsueiurs jour donnée
+    pour un compteur donnee
     in:
         dfDebitVitesseVoiesBddWide : dataframe des trafic et d�bit, avec attributs date_heure, voie, sens, TV, Vmoy_TV
         listJour :  list integer: dayofyear
-        NomVoies liste de string  des noms de voies � consid�rer
-        sens : string: nom du sens consid�r�
+        NomVoies liste de string  des noms de voies à considerer
+        sens : string: nom du sens considere
         largeur : larguer des charts en pixel
         hauteur : hauteur des charts en pixel
     """
